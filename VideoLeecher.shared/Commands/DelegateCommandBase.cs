e16f909be381981794acf724c0753b43635b68a6
Postal86 <amezvrishvili@gmail.com>
2018-02-05 12:02:27 +0400

    Addind Shared  services......

--- VideoLeecher.shared/Commands/DelegateCommandBase.cs
+++ VideoLeecher.shared/Commands/DelegateCommandBase.cs
@@ -30,7 +30,7 @@ namespace VideoLeecher.shared.Commands
             }
 
             _propertiesToObserve = new HashSet<string>();
-            _executeMethod = (arg) => { executeMethod(arg); return Task.Delay(0);  }
+            _executeMethod = (arg) => { executeMethod(arg); return Task.Delay(0); };
             _canExecuteMethod = canExecuteMethod;
         }
 
@@ -50,7 +50,90 @@ namespace VideoLeecher.shared.Commands
 
         #endregion Constructors
 
+        #region Methods
 
+        async void ICommand.Execute(object parameter)
+        {
+            await Execute(parameter);
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            return CanExecute(parameter);
+        }
+
+        protected  virtual async Task Execute(object parameter)
+        {
+            await _executeMethod(parameter);
+        }
+
+        protected virtual bool CanExecute(object parameter)
+        {
+            return _canExecuteMethod(parameter);
+        }
+
+        protected internal void ObservesPropertyInternal<T>(Expression<Func<T>> propertyExpression)
+        {
+            AddPropertyToObserve(PropertySupport.ExtractPropertyName(propertyExpression));
+            HookInpc(propertyExpression.Body as MemberExpression);
+        }
+
+        protected internal void ObservesCanExecuteInternal(Expression<Func<object, bool>> canExecuteExpression)
+        {
+            _canExecuteMethod = canExecuteExpression.Compile();
+            AddPropertyToObserve(PropertySupport.ExtractPropertyNameFromLambda(canExecuteExpression));
+            HookInpc(canExecuteExpression.Body as  MemberExpression);
+        }
+
+        protected void HoopInpc(MemberExpression expression)
+        {
+            if (expression == null)
+                return;
+
+            if (_inpc == null)
+            {
+                if (expression.Expression is ConstantExpression constantExpression)
+                {
+                    _inpc = constantExpression.Value as INotifyPropertyChanged;
+                    if (_inpc != null)
+                        _inpc.PropertyChanged += Inpc_PropertyChanged;
+                }
+            }
+        }
+
+        protected void AddPropertyToObserve(string property)
+        {
+            if (_propertiesToObserve.Contains(property))
+                throw new ArgumentException(String.Format("{0} is already being observed.", property));
+
+            _propertiesToObserve.Add(property);
+
+        }
+
+        private void Inpc_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (_propertiesToObserve.Contains(e.PropertyName))
+                FireCanExecuteChanged();
+        }
+
+        #endregion Methods
+
+        #region Events 
+
+        public virtual event EventHandler CanExecuteChanged;
+
+        protected virtual void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void FireCanExecuteChanged()
+        {
+            OnCanExecuteChanged();
+        }
+
+
+        #endregion Events
 
     }
 }
